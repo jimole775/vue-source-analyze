6e1a6897de758b72c9d55316dc96c6cdde6d50b7
16603013472 <rongchuanda@chinasie.com>
2021-09-16 20:06:56 +0800

    feat: update

--- vue.js
+++ vue.js
@@ -4,9 +4,9 @@
  * Released under the MIT License.
  */
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.Vue = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : // nodejs
+	typeof define === 'function' && define.amd ? define(factory) : // requirejs
+	(global.Vue = factory()); // js
 }(this, (function () { 'use strict';
 
 /*  */
---
+++ 实例化流程/render/vnode.md
@@ -0,0 +1,15 @@
+## 接口，实例化的参数几乎和createElement对应
+VNode (
+  tag?: string,
+  data?: VNodeData,
+  children?: ?Array<VNode>,
+  text?: string,
+  elm?: Node,
+  context?: Component,
+  componentOptions?: VNodeComponentOptions,
+  asyncFactory?: Function
+)
+
+## 
+vnode可以当成是vdom树的一个描述对象
+
